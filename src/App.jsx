b8e11b51df9bdc13a5d9462b1aef86e91c181aec
nikita3006 <nikitanegi3011@gmail.com>
2024-06-20 01:28:30 +0530

    this is project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+function App(){
+  return (
+    <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+    <h1>
+      Hello
+    </h1>
+    </Box>
+  )
+}
+export default App
